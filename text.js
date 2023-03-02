67e42892030a19cf428ab26301c3bcfd47a8276c
Uday Atthuluri <59479790+Atthuluri-udaykumar@users.noreply.github.com>
2023-03-02 22:37:16 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,83 +1,48 @@
-import fetch from "cross-fetch";
-import { ENV_CONFIG } from "../constant/env-config";
+import { Injectable } from '@angular/core';
+import {HttpHeaders} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { EnvConfigService } from '../shared/services/env-config/env-config.service';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _url: string = this.envConfig.getEnvConfig().baseUrl;
+  private _authorization = this.envConfig.getEnvConfig().authorization;
+  private _xApiKey = this.envConfig.getEnvConfig().xapikey;
 
+  httpOptions: any = {
+    headers: new HttpHeaders({
+      'x-api-key': this._xApiKey,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': this._authorization
 
-class AuthServiceController {
+    })
+  };
 
-
-  public envs: string[] = [
-    'sit1', 'sit2', 'sit3', 'local'
-  ];
-
-  public getHostName(): string {
-    return window.location.hostname;
+  constructor(
+    private http: HttpClient, public envConfig: EnvConfigService
+    ) {
   }
-
-  public getEnv(): string {
-    let env: string = 'prod';
-    const host: string = this.getHostName();
-
-    for (const i of this.envs) {
-      if (host && host.includes(i)) {
-        env = i;
-        break;
-      }
+  onLoginAuthentication(data: any): Observable<any> {
+    if(this.envConfig.getEnvConfig().env == "local"){
+      return this.http.get('./assets/api/loginAuthentication.json');
     }
-    return env
+   // return this.http.post(`${this._url}/specialty/loginservice/v1/authentication`, data, this.httpOptions); 
+    return this.http.post(`${this._url}/apims/specialty/loginservice/v1/authentication`, data, this.httpOptions);
   }
 
-  public getEnvConfig(): any {
-    const env: string = this.getEnv() || 'prod';
 
-    return ENV_CONFIG[env];
+
+  getPatientProfileService(payload: object): Observable<any> {  
+    if(this.envConfig.getEnvConfig().env == "local"){
+      return this.http.get('./assets/api/patientProfile.json');
+    }  
+   // return this.http.post(`${this._url}/specialty/loginservice/details/v1/patientprofile`, payload, this.httpOptions);
+    return this.http.post(`${this._url}/apims/specialty/loginservice/details/v1/patientprofile`, payload, this.httpOptions);
   }
 
-  private config =this.getEnvConfig();
   
-  private baseUrl='https://sit1-api.cvshealth.com/'
-
-
-  private getHeaders(type:string,idToken:any):any{
-    return ({
-      'Content-Type': type,
-      'Accept': '*/*',
-      'Authorization':`Bearer ${idToken}`,
-      'x-api-key': '1M3aGZyeBYEJFFK84TCmFX230Qtp3rZG',
-      'Cookie': "1M3aGZyeBYEJFFK84TCmFX230Qtp3rZG",
-
-    });
- }  
- private readonly getLogOutReqInit = (idToken:any): RequestInit => {
-    return {
-      method: "DELETE",
-      headers: this.getHeaders('application/json',idToken)
-    }
-  }
-
-  private readonly getRefreshReqInit = (idToken:any): RequestInit => {
-    return {
-      method: "GET",
-      headers: this.getHeaders('application/x-www-form-urlencoded',idToken),
-      // body: `grant_type=refresh_token&client_id=${this.config.keyCloakConfig.clientId}`
-    }
-  }
-  
-  public logOut(idToken:any): Promise<any> {
-    const logoutUrl: string = `${this.baseUrl}specialty/userservice/v1/logout`;
-      return fetch(logoutUrl, this.getLogOutReqInit(idToken));
-  }
-
-  public refreshToken(idToken:any): Promise<any> {
-    const tokenUrl: string = `${this.baseUrl}specialty/userservice/v1/refreshtoken`;
-    return fetch(tokenUrl, this.getRefreshReqInit(idToken));
-  }
-
-  public set envConfig(config: any) {
-    this.config = config;
-  }
-  public get envConfig(): any {
-    return this.config;
-  }
 }
-
-export const AuthService = new AuthServiceController();
